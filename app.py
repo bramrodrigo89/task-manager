10900aa859e6c8935b11a9bf683e583aa4c207fa
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-05-08 22:20:43 +0000

    added functionality for editing and saving edited tasks

--- app.py
+++ app.py
@@ -27,5 +27,26 @@ def insert_task():
     task_collection.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),port=int(os.environ.get('PORT')),debug=True)
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task=mongo.db.tasks.find_one({'_id':ObjectId(task_id)})
+    all_categories=mongo.db.categories.find()
+    return render_template("edittask.html", categories=all_categories, task=the_task)
+
+@app.route('/update_task/<task_id>', methods=['POST'])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update( {'_id': ObjectId(task_id)},
+    {
+        'task_name':request.form.get('task_name'),
+        'category_name':request.form.get('category_name'),
+        'task_description': request.form.get('task_description'),
+        'due_date': request.form.get('due_date'),
+        'is_urgent':request.form.get('is_urgent')
+    })
+    return redirect(url_for('get_tasks'))
+    
+if __name__=='__main__':
+    app.run(host=os.getenv("IP","0.0.0.0"),
+        port=int(os.getenv("PORT","5000")),
+        debug=True)
